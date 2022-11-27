c3a656ae61b281d5f2f4728db43f7892aa562b21
Sushma <sushma.gayatri451@gmail.com>
2022-11-27 18:11:19 +0530

    Vpc

--- route-table.tf
+++ route-table.tf
@@ -1,3 +1,4 @@
+# Creates public Route-table
 resource "aws_route_table" "public-rt" {
   vpc_id = aws_vpc.main.id
 
@@ -5,7 +6,38 @@ resource "aws_route_table" "public-rt" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+    route {
+    cidr_block                  = var.VPC_CIDR
+    vpc_peering_connection_id   = aws_vpc_peering_connection.peer.id
+  }
   tags = {
     Name = "${var.ENV}-pub-route-table"
   }
+}
+
+# Attaches public route-table to public subnets
+
+resource "aws_route_table_association" "public-rt-association" {
+  count          = length(aws_subnet.public.*)
+  subnet_id      = element(aws_subnet.public.*.id_subnet, count.index)
+  route_table_id = aws_route_table.public-rt.id
+}
+
+# Creates private Route-table
+resource "aws_route_table" "private-rt" {
+  vpc_id = aws_vpc.main.id
+# Accepts the traffic only from the default VPC
+    route {
+    cidr_block                  = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id   = aws_vpc_peering_connection.peer.id
+  }
+  tags = {
+    Name = "${var.ENV}-pub-route-table"
+  }
+}
+
+resource "aws_route_table_association" "private-rt-association" {
+  count          = length(aws_subnet.private.*)
+  subnet_id      = element(aws_subnet.private.*.id_subnet, count.index)
+  route_table_id = aws_route_table.private-rt.id
 }
